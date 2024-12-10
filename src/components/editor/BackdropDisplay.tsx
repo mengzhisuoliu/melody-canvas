78626462b55b9075236ad36d6c06803756e53361
Rylan <1217013295@qq.com>
2024-12-10 18:07:38 +0800

    🔧 chore: replace <SelectInput> with <Select/> in BackdropDisplay

--- src/components/editor/BackdropDisplay.tsx
+++ src/components/editor/BackdropDisplay.tsx
@@ -1,46 +1,21 @@
-import { useState } from "react";
-import { ColorPickerPanel, SelectInput } from "tdesign-react";
+import { ColorPickerPanel, Select } from "tdesign-react";
 
+import { formatSelectOptions } from "@/libs/common/toolkit";
 import useCanvasStore from "@/stores/canvasStore";
 
 const BackdropDisplay: React.FC = () => {
   const { backdrop, setBackdrop } = useCanvasStore();
-  const [popupVisible, setPopupVisible] = useState(false);
-
-  const ratioOptions = ["16:9", "9:16", "1:1"]
-    .filter((item) => item !== backdrop.ratio)
-    .map((item) => ({ content: item }));
 
   return (
     <>
       <div className="flex flex-col space-y-8">
         {/* 画布比例 */}
-        <div className="card">
+        <div className="card flex items-center">
           <span className="card-title mr-4">Ratio</span>
-          <SelectInput
-            suffix={<div className="i-gridicons:chevron-down"></div>}
-            popupVisible={popupVisible}
-            onPopupVisibleChange={(val) => setPopupVisible(val)}
+          <Select
             style={{ width: "65%" }}
             value={backdrop.ratio}
-            panel={
-              <ul className="text-right text-base cursor-pointer">
-                {ratioOptions.map((item) => (
-                  <li
-                    className="px-1 rounded-md hover:(bg-emerald-100 dark:bg-dark-100)"
-                    key={item.content}
-                    onClick={() => {
-                      setBackdrop({
-                        ratio: item.content
-                      });
-                      setPopupVisible(false);
-                    }}
-                  >
-                    {item.content}
-                  </li>
-                ))}
-              </ul>
-            }
+            options={formatSelectOptions(["16:9", "9:16", "1:1"])}
           />
         </div>
 

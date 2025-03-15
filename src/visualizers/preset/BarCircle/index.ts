09b1383cd3b709ec83e63735601adf17c6e54f84
Rylan <rylanbot@qq.com>
2025-03-15 20:07:12 +0800

    🐛 fix(BarCircle): update only height to prevent overlap

--- src/visualizers/preset/BarCircle/index.ts
+++ src/visualizers/preset/BarCircle/index.ts
@@ -7,7 +7,6 @@ import Builder from "../../core/Builder";
  * 柱状环形
  */
 class BarCircle extends Builder {
-  private readonly barWidth = 4;
   private readonly barHeight = 20;
 
   constructor(count: number, color: string) {
@@ -16,10 +15,11 @@ class BarCircle extends Builder {
 
   protected createElements(groupWidth: number, groupHeight: number) {
     const bars: Rect[] = [];
-    const orbitRadius = this.calcOrbitRadius(groupWidth, groupHeight);
+    const orbitRadius = (Math.min(groupWidth, groupHeight) - this.barHeight - 1) / 2;
+    const barWidth = 10 - Math.log2(this.count);
 
     for (let i = 0; i < this.count; i++) {
-      const angle = this.calcAngle(i);
+      const angle = (i / this.count) * Math.PI * 2;
       const x = groupWidth / 2 + orbitRadius * Math.cos(angle);
       const y = groupHeight / 2 + orbitRadius * Math.sin(angle);
       const rotationAngle = (angle * 180) / Math.PI + 90;
@@ -27,7 +27,7 @@ class BarCircle extends Builder {
       const bar = new Rect({
         left: x,
         top: y - this.barHeight / 2,
-        width: this.barWidth,
+        width: barWidth,
         height: this.barHeight,
         fill: this.colorMap[i],
         originX: "center",
@@ -59,36 +59,22 @@ class BarCircle extends Builder {
     if (!frequency) return;
 
     const MIN_SCALE = 0.5;
-    const MAX_SCALE = 2;
+    const MAX_SCALE = 2.5;
 
     const objHeights = normalize(frequency, MIN_SCALE, MAX_SCALE);
-    const orbitRadius = this.calcOrbitRadius(this.group.width, this.group.height);
 
     this.group.getObjects().forEach((bar, i) => {
       if (bar.type !== "rect") return;
 
-      const angle = this.calcAngle(i);
       const freqIndex = Math.floor((i / this.count) * objHeights.length);
       const scale = objHeights[freqIndex];
       const newHeight = this.barHeight * scale;
-      const newY = orbitRadius * Math.sin(angle);
 
       bar.set({
-        height: newHeight,
-        top: newY - (newHeight - this.barHeight + 1) / 2
+        height: newHeight
       });
     });
   }
-
-  /* ----- 通用的计算逻辑 ----- */
-  private calcOrbitRadius(groupWidth: number, groupHeight: number) {
-    return (Math.min(groupWidth, groupHeight) - this.barHeight - 1) / 2;
-  }
-
-  private calcAngle(index: number) {
-    return (index / this.count) * Math.PI * 2;
-  }
-  /* ------------------------- */
 }
 
 export default BarCircle;
